8f3a4cb118a4f6ea10d42a58e5ef3671fac231ca
jackbackrack <jrb@pobox.com>
2023-10-07 10:59:44 -0700

    add import/export to admin interface

--- piece/admin.py
+++ piece/admin.py
@@ -1,7 +1,11 @@
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
 
 from .models import Artist, Piece, Show
 
-admin.site.register(Piece)
-admin.site.register(Artist)
-admin.site.register(Show)
+class ImportExportAdmin(ImportExportModel, admin.ModelAdmin) :
+    pass
+
+admin.site.register(Piece, ImportExportAdmin)
+admin.site.register(Artist, ImportExportAdmin)
+admin.site.register(Show, ImportExportAdmin)
