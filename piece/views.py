19bc825191cbe8403a239a90e96c75cfaa86c9cd
jackbackrack <jrb@pobox.com>
2025-09-24 13:59:26 -0700

    order shows by reverse chronological order

--- piece/views.py
+++ piece/views.py
@@ -16,6 +16,11 @@ def detail(request, pk):
 class ShowListView(ListView):
     model = Show
     template_name = "piece/show_list.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        shows = Show.objects.order_by('-start')
+        context['shows'] = shows
+        return context
 
 class ShowDetailView(DetailView):
     model = Show
